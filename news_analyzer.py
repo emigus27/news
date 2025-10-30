14d680c01061a407fc016aaacb9a89869e3a71f7
emigus27 <folkeg00@gmail.com>
2025-10-30 14:04:22 +0100

    did some updates

--- news_analyzer.py
+++ news_analyzer.py
@@ -2,4 +2,28 @@ import pandas as pd
 from textblob import TextBlob
 
 
+def article_per_date(df):
+    df['publishedAt'] = pd.to_datetime(df['publishedAt']).dt.date
+    return df.groupby('publishedAt').size().reset_index(name='article_count')
+
+
+def analyze_sentiment(text):
+        blob = TextBlob(text)
+        return blob.sentiment.polarity
+
+def sentiment_category(polarity):
+    if polarity > 0.1:
+        return 'positive'
+    elif polarity < -0.1:
+        return 'negative'
+    else:
+        return 'neutral'
+
+def sentiment_analysis(df):
+    df['sentiment'] = df['description'].fillna('').apply(analyze_sentiment)
+    df['sentiment_category'] = df['sentiment'].apply(sentiment_category)
+    return df
+
 df = pd.read_csv("news_articles.csv")
+df = sentiment_analysis(df)
+
--- news_fetcher.py
+++ news_fetcher.py
@@ -10,26 +10,21 @@ if not api_key:
     raise ValueError("NEWS_API_KEY not found in .env file")
 
 
-
-def fetch_news(api_key, days=5, max_pages=4):
-
-    to_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d") 
-    from_date = (datetime.now() - timedelta(days=days+1)).strftime("%Y-%m-%d")  
-
-    page = 1
+def fetch_news(api_key, days=7, query="Sweden"):
     all_articles = []
 
-    while page <= max_pages:
+    for i in range(days):
+        to_date = (datetime.now() - timedelta(days=i+1)).strftime("%Y-%m-%d")
+        from_date = (datetime.now() - timedelta(days=i+2)).strftime("%Y-%m-%d")
         url = (
         f"https://newsapi.org/v2/everything?"
-        f"q=Sweden&"
+        f"q={query}&"
         f"searchIn=title,description&"
         f"language=en&"
         f"from={from_date}&"
         f"to={to_date}&"
         f"sortBy=popularity&"
         f"pageSize=100&"
-        f"page={page}&"
         f"apiKey={api_key}")
 
         response = requests.get(url)
@@ -38,19 +33,18 @@ def fetch_news(api_key, days=5, max_pages=4):
 
         data = response.json()
         articles = data.get("articles", [])
-        
-        if not articles:
-            break
         all_articles.extend(articles)
-        page += 1
-
 
     return pd.DataFrame(all_articles)
 
 
-df = fetch_news(api_key)
-print('Fetched', len(df), 'articles')
-df.to_csv("news_articles.csv", index=False)
+try:
+    df = fetch_news(api_key)
+    df.to_csv("news_articles.csv", index=False)
+except Exception as e:
+    if os.path.exists("news_articles.csv"):
+        df = pd.read_csv("news_articles.csv")
+    else:
+        raise
 
 
-    
